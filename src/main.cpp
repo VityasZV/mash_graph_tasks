eaef5d45091c32facf5d42274be302d4b17a8df4
Vityas <viktor.zadyabin@mail.ru>
2020-03-02 20:50:08 +0300

    its simple but it works on PC

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,43 @@
 #include <iostream>
-#include <cstdint>
+#include <stdlib.h>
+#define GLFW_DLL
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-#include <string>
-#include <vector>
-#include <unordered_map>
-
-#include "GLFW/glfw3.h"
-#include "Bitmap/Bitmap.h"
-
-const uint32_t RED   = 0x000000FF;
-const uint32_t GREEN = 0x0000FF00;
-const uint32_t BLUE  = 0x00FF0000;
-const int width = 1920, height = 1080;
-
-int main(int argc, const char** argv)
+void key_callback(GLFWwindow* window,int key,int scancode, int action,int mode)
 {
-  std::unordered_map<std::string, std::string> cmdLineParams;
-  std::cout << GLFW_KEY_0 << std::endl;
-  for(int i=0; i<argc; ++i)
-  {
-    std::string key(argv[i]);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window,GL_TRUE);
+}
 
-    if(key.size() > 0 && key[0]=='-')
+int main() {
+    int width, height;
+//    float colorR,colorB,colorG;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(1200,800,"LearnOpenGl", nullptr, nullptr);
+    if (window == nullptr)
     {
-      if(i != argc-1) // not last argument
-      {
-        cmdLineParams[key] = argv[i+1];
-        i++;
-      }
-      else
-        cmdLineParams[key] = "";
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
     }
-  }
-
-  std::string outFilePath = "zout.bmp";
-  if(cmdLineParams.find("-out") != cmdLineParams.end())
-    outFilePath = cmdLineParams["-out"];
-
-  int sceneId = 0;
-  if(cmdLineParams.find("-scene") != cmdLineParams.end())
-    sceneId = atoi(cmdLineParams["-scene"].c_str());
-
-  uint32_t color = 0;
-  if(sceneId == 1)
-    color = RED;
-  else if(sceneId == 2)
-    color = RED | GREEN;
-  else if(sceneId == 3)
-    color = BLUE;
-  
-  std::vector<uint32_t> image(width*height); 
-  for(auto& pixel : image)
-    pixel = color;
-
-  BMP::SaveBMP(outFilePath.c_str(), image.data(), width, height);
-
-  std::cout << "end." << std::endl;
-  return 0;
+    glfwGetFramebufferSize(window,&width,&height);
+    glViewport(0,0,width,height);
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSetKeyCallback(window, key_callback);
+        glfwPollEvents();
+//        glClearColor((double)(rand())/RAND_MAX,(double)(rand())/RAND_MAX,(double)(rand())/RAND_MAX,(double)(rand())/RAND_MAX);
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+    glfwTerminate();
+    return 0;
 }
